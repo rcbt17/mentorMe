963cb888ce4ec3619c7b350f1369a1f042dea0fa
matteo argenio <matteargy@gmail.com>
2023-06-05 16:30:45 +0200

    done it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_124651) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_141800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_124651) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "course_subscriptions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_course_subscriptions_on_course_id"
+    t.index ["user_id"], name: "index_course_subscriptions_on_user_id"
   end
 
   create_table "courses", force: :cascade do |t|
@@ -81,6 +90,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_124651) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "course_subscriptions", "courses"
+  add_foreign_key "course_subscriptions", "users"
   add_foreign_key "courses", "users"
   add_foreign_key "lessons", "courses"
 end
