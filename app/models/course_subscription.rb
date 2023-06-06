1ccfee9bb1d1d835d7d213957d8ea0a28e42135b
Robin Ciubotaru <robinciubotaru17@gmail.com>
2023-06-06 14:23:57 +0200

    Error fixed on subs

--- app/models/course_subscription.rb
+++ app/models/course_subscription.rb
@@ -3,6 +3,6 @@ class CourseSubscription < ApplicationRecord
   belongs_to :course
 
   def self.user_subscribed?(user, course)
-    user.course_subscriptions.where(course_id: course.id, user_id: user.id).count.zero?
+    !user.course_subscriptions.where(course_id: course.id, user_id: user.id).count.zero?
   end
 end
