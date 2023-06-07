ccc96ab04428eb39b0faca241778afdbd2bfe43e
Robin Ciubotaru <robinciubotaru17@gmail.com>
2023-06-07 15:29:13 +0200

    Plyr works ok

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ApplicationRecord
   belongs_to :lesson
   belongs_to :user
-  has_many :posts
+  has_many :posts, dependent: :destroy
   validates :title, presence: true
   validates :content, presence: true
 end
---
+++ db/migrate/20230607125624_add_timestamp_to_topics.rb
@@ -0,0 +1,5 @@
+class AddTimestampToTopics < ActiveRecord::Migration[7.0]
+  def change
+    add_column :topics, :timestamp, :integer
+  end
+end
