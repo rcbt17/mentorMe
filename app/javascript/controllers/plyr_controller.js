daa4f2a0ca930c0e5472ea774cb49f73e7b3784d
Robin Ciubotaru <robinciubotaru17@gmail.com>
2023-06-09 14:14:34 +0200

    Okeroni

--- app/javascript/controllers/plyr_controller.js
+++ app/javascript/controllers/plyr_controller.js
@@ -3,15 +3,10 @@ import Plyr from 'plyr';
 
 // Connects to data-controller="plyr"
 export default class extends Controller {
+
+
   connect() {
-    player = new Plyr('#course-player');
+    let player = new Plyr('#course-player');
+
   }
-}
-
-
-let aud = document.getElementById("course-player");
-aud.ontimeupdate = function () { getVideoTime() };
-
-function getVideoTime() {
-  document.getElementById("timestamp").value = Math.round(aud.currentTime);
 }
