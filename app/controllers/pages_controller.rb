f4e36b91d33bcd85b48b03e8cece3a36372a2c61
matteo argenio <matteargy@gmail.com>
2023-06-14 16:31:08 +0200

    implemented all the stats for mentor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,24 +2,19 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   include ApplicationHelper
 
-
   def home
   end
-
-
 
   def help
   end
 
   def statistics
     @user = User.find(params[:id])
-    @user_type = "student"
+    @user_type = @user.courses.count.positive? ? "mentor" : "student"
     @total_lessons = calculate_total_lessons
-    if @user.courses.count.positive?
-      @user_type = "mentor"
-    else
-      user_type = "student"
-    end
+    @total_views = calculate_total_views
+    @total_subscribers = calculate_total_subscribers
+    @average_reviews = calculate_average_review
   end
 
   def calculate_review(course)
@@ -40,5 +35,39 @@ class PagesController < ApplicationController
       total_lessons += course.lessons.count
     end
     total_lessons
+  end
+
+  def calculate_total_views
+    total_views = 0
+    @user.courses.each do |course|
+      total_views += course.lessons.joins(:user_lesson_views).count
+    end
+    total_views
+  end
+
+  def calculate_total_subscribers
+    total_subscribers = 0
+    @user.courses.each do |course|
+      total_subscribers += CourseSubscription.where(course: course).count
+    end
+    total_subscribers
+  end
+
+  def calculate_average_review
+      total_courses = 0
+      total_reviews = 0
+    @user.courses.each do |course|
+      course.reviews.each do |review|
+        total_reviews += review.stars
+        total_courses += 1
+      end
+    end
+    if total_reviews.zero?
+      return "Not rated yet"
+    else
+      average_reviews = (total_reviews.to_f / total_courses)
+        return average_reviews.round(2)
+  end
+
   end
 end
